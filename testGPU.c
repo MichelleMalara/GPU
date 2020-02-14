bc9fc695b94670368ed68b41df89a641f479da63
Rémy Lavainne <remy.lavainne@ensimag.grenoble-inp.fr>
2020-02-14 16:41:01 +0100

    debut programme

--- testGPU.c
+++ testGPU.c
@@ -7,6 +7,43 @@
 #include <math.h>
 
 #define QTE_DONNEES 20
+char* read_file(char* fileName){
+
+    FILE* fichier = NULL;
+    char chaine[5000] = "";
+    char* result = NULL;
+    fichier = fopen(fileName, "r");
+    int i=0;
+    int compteur=0;
+    int compteur2=0;
+
+    if (fichier != NULL)
+    {
+        while (fgets(chaine, 5000, fichier) != NULL) // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+        {
+            i=0;
+            while(chaine[i]!='\n'){
+                i++;
+            }
+            compteur=compteur+i+1;
+            result = realloc(result, sizeof(char)*compteur);
+            i=0;
+            while(chaine[i]!='\n'){
+                result[compteur2+i]=chaine[i];
+                i++;
+            }
+            result[compteur-1]=chaine[i];
+            compteur2 = compteur;
+        }
+
+        fclose(fichier);
+    }
+    else{
+        printf("Impossible d'ouvrir le fichier");
+        exit(1);
+    }
+    return result;
+}
 int main (int argc, const char * argv[])
 {
 
@@ -32,18 +69,18 @@ int main (int argc, const char * argv[])
         "}\n"
     };
     const char *getPath = {
-        "#include <../../user/7/lavainnr/Documents/cours/GPU/point.h>\n"
-        "#include <../../user/7/lavainnr/Documents/cours/GPU/listIndice.h>\n"
-        "#include <../../user/7/lavainnr/Documents/cours/GPU/listPoint.h>\n"
-        "#include <../../user/7/lavainnr/Documents/cours/GPU/listIndiceList.h>\n"
+        "#include \"point.h\"\n"
+        "#include \"listIndice.h\"\n"
+        "#include \"listPoint.h\"\n"
+        "#include \"listIndiceList.h\"\n"
         "__kernel void getPath(__global listPoint2D *input, __global listIndiceList *output, __global int *nbProcess, __global listIndice *pointForPath)\n"
         "{\n"
-        " int id = get_global_id(0);\n"
+        /*" int id = get_global_id(0);\n"
         " if(id<*nbProcess-1){\n"
         " listPoint2D projec;\n"
             " projec = projectionWithIndice(*input,getIndice(*pointForPath,id));\n"
             //" setListIndice(output, Convex_HullIndice(projec), id);\n"
-          "}\n"
+          "}\n"*/
         "}\n"
     };
 
@@ -96,9 +133,25 @@ int main (int argc, const char * argv[])
     // Création de la file d'exécution pour le CPU
     file_execution = clCreateCommandQueue(contexte, TOUS_peripheriqueID, 0, &codeErreur);
 
+    /*static const char *sources[5] = {
+        read_file("point.c"),
+        read_file("listPoint.c"),
+        read_file("listIndice.c"),
+        read_file("listIndiceList.c"),
+        getPath
+    };*/
+    const char *sources[5] = {
+        read_file("point.c"),
+        read_file("listPoint.c"),
+        read_file("listIndice.c"),
+        read_file("listIndiceList.c"),
+        getPath
+    };
+
     // Construire le programme avec la fonction auCarre pour le CPU
-    programme = clCreateProgramWithSource(contexte, 1, (const char**)&getPath, NULL, &codeErreur);
-    //programme = clCreateProgramWithSource(contexte, 1, (const char**)&maFonctionAuCarre, NULL, &codeErreur);
+    //programme = clCreateProgramWithSource(contexte, 5, (const char**)&sources, NULL, &codeErreur);
+    //programme = clCreateProgramWithSource(contexte, 1, (const char**)&getPath, NULL, &codeErreur);
+    programme = clCreateProgramWithSource(contexte, 1, (const char**)&maFonctionAuCarre, NULL, &codeErreur);
 
     // Compilation du programme
     codeErreur = clBuildProgram(programme, TOUS_qtePeripheriques, &TOUS_peripheriqueID, include_path, NULL, NULL);
@@ -114,7 +167,6 @@ int main (int argc, const char * argv[])
         return 1;
     }
 
-    printf("Test");
     // Association des variables de données avec le tampon d'échange
     list_buffer = clCreateBuffer(contexte, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
             sizeof(listPoint2D), &copyList, &codeErreur);
@@ -132,17 +184,17 @@ int main (int argc, const char * argv[])
 
 
     // Construire le noyau
-    //noyau = clCreateKernel(programme, "auCarre", &codeErreur);
-    noyau = clCreateKernel(programme, "getPath", &codeErreur);
+    noyau = clCreateKernel(programme, "auCarre", &codeErreur);
+    //noyau = clCreateKernel(programme, "getPath", &codeErreur);
 
     // Associer les tampons d'échanges avec
     // les arguments des fonctions à paralléliser
-    //codeErreur = clSetKernelArg(noyau, 0, sizeof(input_buffer), &input_buffer);
-    //codeErreur = clSetKernelArg(noyau, 1, sizeof(output_buffer), &output_buffer);
-    codeErreur = clSetKernelArg(noyau, 0, sizeof(list_buffer), &list_buffer);
+    codeErreur = clSetKernelArg(noyau, 0, sizeof(input_buffer), &input_buffer);
+    codeErreur = clSetKernelArg(noyau, 1, sizeof(output_buffer), &output_buffer);
+    /*codeErreur = clSetKernelArg(noyau, 0, sizeof(list_buffer), &list_buffer);
     codeErreur = clSetKernelArg(noyau, 1, sizeof(path_buffer), &path_buffer);
     codeErreur = clSetKernelArg(noyau, 2, sizeof(nbProcess_buffer), &nbProcess_buffer);
-    codeErreur = clSetKernelArg(noyau, 3, sizeof(pointForPath_buffer), &pointForPath_buffer);
+    codeErreur = clSetKernelArg(noyau, 3, sizeof(pointForPath_buffer), &pointForPath_buffer);*/
 
     // Mettre le noyau dans la file d'execution
     size_t dimensions_globales[] = { QTE_DONNEES, 0, 0 };
@@ -150,13 +202,12 @@ int main (int argc, const char * argv[])
             dimensions_globales, NULL, 0, NULL, NULL);
 
     // Récupération des résultats dans le tampon
-    clEnqueueReadBuffer(file_execution, path_buffer, CL_TRUE,
-            0, sizeof(int) * QTE_DONNEES, &paths, 0, NULL, NULL);
-    //clEnqueueReadBuffer(file_execution, output_buffer, CL_TRUE,
-    //        0, sizeof(int) * QTE_DONNEES, outputData, 0, NULL, NULL);
+    //clEnqueueReadBuffer(file_execution, path_buffer, CL_TRUE,
+    //        0, sizeof(int) * QTE_DONNEES, &paths, 0, NULL, NULL);
+    clEnqueueReadBuffer(file_execution, output_buffer, CL_TRUE,
+            0, sizeof(int) * QTE_DONNEES, outputData, 0, NULL, NULL);
 
     // Affichage des résultats
-    displayListIndiceList(paths);
     printf("\nOuahhh\n");
     printf("\n\n\n >>>>> Affichage des resultats <<<<<");
     printf("\n\nContenu de la variable --inputData--\n");
